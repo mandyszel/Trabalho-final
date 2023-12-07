27487d078edb037ad654183e81d295a1357fb1d7
Amanda Szelag <amandaszelag0@gmail.com>
2023-12-07 14:59:01 -0300

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Olá Amanda");
+});
+
+app.listen(3000, function () {
+  console.log("Conexão inicializada na porta 300");
+});
