a918a5f93bc04f457322bffb5a321f27c56c397d
Amanda Szelag <amandaszelag0@gmail.com>
2023-12-08 13:50:11 -0300

    apaguei rotas de teste

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ app.get("/", function (req, res) {
   res.send("Olá Amanda");
 });
 
-app.get("/mensagem", function (req, res) {
-  res.send("Essa mensagem é automática");
-});
-
-app.get("/layout", function (req, res) {
-  res.send("Esta é o novo layout");
-});
-
 app.listen(3000, function () {
   console.log("Conexão inicializada na porta 3000");
 });
