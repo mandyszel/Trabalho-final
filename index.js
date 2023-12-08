b61e891cd15e6c2f43d666ed69f166fe9ab2d1c3
Amanda Szelag <amandaszelag0@gmail.com>
2023-12-08 13:36:43 -0300

    adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/mensagem", function (req, res) {
   res.send("Essa mensagem é automática");
 });
 
+app.get("/sobre", function (req, res) {
+  res.send("Esta pagina esta sendo desenvolvida por amanda");
+});
+
 app.listen(3000, function () {
   console.log("Conexão inicializada na porta 300");
 });
