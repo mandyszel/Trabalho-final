cb461a384df4369688e01122e1bce8654b57d381
Amanda Szelag <amandaszelag0@gmail.com>
2023-12-08 14:56:16 -0300

    cricao das rotas get e post do add

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.get("/usuarios", function (req, res) {
   });
 });
 
+app.get("/add", function (req, res) {
+  res.render("adiciona.ejs");
+});
+
+app.post("/add", function (req, res) {
+  console.log("deu certo");
+});
+
 app.listen(3000, function () {
   console.log("Conexão inicializada na porta 3000");
 });
