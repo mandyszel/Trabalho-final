cb34acd2cd7c36e175ec88e753aaed8849f83d31
Amanda Szelag <amandaszelag0@gmail.com>
2023-12-09 18:00:48 -0300

    vairas alteraçoes do mogno db pra depois voltar pro mesmo

---
+++ config/conexao.js
@@ -0,0 +1,16 @@
+/* const mongoose = require('mongoose')
+
+//mongoose.connect('mongodb://localhost:27017/loop')
+//mongoose.connect('mongodb://ifsul:ifsul@ac-pcra3vd-shard-00-00.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-01.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-02.ajiapmy.mongodb.net:27017/?ssl=true&replicaSet=atlas-14e4b2-shard-0&authSource=admin&retryWrites=true&w=majority')
+
+const uri = "mongodb://localhos/;27017/trabalho"
+//mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
+
+module.exports = mongoose */
+
+const mongoose = require('mongoose')
+
+//mongoose.connect('mongodb://localhost:27017/loop')
+mongoose.connect('mongodb://ifsul:ifsul@ac-pcra3vd-shard-00-00.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-01.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-02.ajiapmy.mongodb.net:27017/?ssl=true&replicaSet=atlas-14e4b2-shard-0&authSource=admin&retryWrites=true&w=majority')
+
+module.exports = mongoose
